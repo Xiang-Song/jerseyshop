bb7db3efd487cad8654a3d01a36a744bd8c7fad2
Xiang Song <himmelsong@gmail.com>
2020-06-03 22:07:09 -0700

    add reducer.js

---
+++ src/constants/constants.js
@@ -0,0 +1,12 @@
+export const ADD_TO_CART = "ADD_TO_CART";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const ADD_QUANTITY = "ADD_QUANTITY";
+export const SUB_QUANTITY = 'SUB_QUANTITY';
+
+// module.exports = {
+// ADD_TO_CART,
+// DELETE_ITEM,
+// ADD_QUANTITY,
+// SUB_QUANTITY
+// };
+
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,25 +1,57 @@
+import asa from '../JerseyImg/Arsenal-away.JPG';
+import ash from '../JerseyImg/Arsenal-home.JPG';
+import che from '../JerseyImg/Chelsea.JPG';
+import liv from '../JerseyImg/Liverpool.JPG';
+import mct from '../JerseyImg/Mancity.JPG';
 import mua from '../JerseyImg/MU-away.JPG';
 import mut from '../JerseyImg/MU-third.JPG';
 import rma from '../JerseyImg/RM-away.JPG';
 import rmh from '../JerseyImg/RM-home.JPG';
-import asa from '../JerseyImg/Arsenal-away.JPG';
-import ash from '../JerseyImg/Arsenal-home.JPG';
+import ttn from '../JerseyImg/Tottenham.JPG';
+import { ADD_TO_CART, DELETE_ITEM } from '../constants/constants.js';
 
 const initState = {
     items:[
-         {id:1, title:"Manchester United Away Jersey", Price:59.99, img:mua},
-         {id:2, title:"Manchester United Third Jersey", Price:59.99, img:mut},
-         {id:3, title:"Real Madrid Away Jersey", Price:69.99, img:rma},
-         {id:4, title:"Real Madrid Home Jersey", Price:79.99, img:rmh},
-         {id:5, title:"Arsenal Away Jersey", Price:59.99, img:asa},
-         {id:6, title:"Arsenal Home Jersey", Price:69.99, img:ash}
+        {id:1, title:"Manchester United Away Jersey", price:59.99, img:mua},
+        {id:2, title:"Manchester United Third Jersey", price:59.99, img:mut},
+        {id:3, title:"Real Madrid Away Jersey", price:69.99, img:rma},
+        {id:4, title:"Real Madrid Home Jersey", price:79.99, img:rmh},
+        {id:5, title:"Arsenal Away Jersey", price:59.99, img:asa},
+        {id:6, title:"Arsenal Home Jersey", price:69.99, img:ash},
+        {id:7, title:"LiverPool Home Jersey", price:69.99, img:liv},
+        {id:8, title:"Tottenham Home Jersey", price:59.99, img:ttn},
+        {id:9, title:"Chelsea Home Jersey", price:59.99, img:che},
+        {id:10, title:"Manchester City Home Jersey", price:59.99, img:mct}
     ],
-    addItem: [],
+    addedItems: [],
     total: 0
 }
 
 const reducer = (state = initState, action) => {
-    return state
+    switch (action.type) {
+        case ADD_TO_CART:
+            let selectedItem = state.items.find(item => item.id === action.id);
+            let cartHasItem = state.addedItems.find(item => item.id === action.id);
+            if (cartHasItem) {
+                selectedItem.quantity += 1;
+                return {
+                    ...state,
+                    total: state.total + selectedItem.price,
+                };
+            }
+            else {
+                selectedItem.quantity = 1;
+                return {
+                    ...state,
+                    addedItems: [...state.addedItems, selectedItem],
+                    total: state.total + selectedItem.price,
+                };
+            }
+            
+        default: {
+            return state;
+        }
+    }
 }
 
 export default reducer;
