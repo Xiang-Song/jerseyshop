e1727a5cbea396dc93e9c5b62cf3bff2bad721a0
Xiang Song <himmelsong@gmail.com>
2020-06-03 22:07:29 -0700

    App.js update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import NavBar from './NavBar';
+import JerseyListContainer from '../containers/JerseyListContainer.js';
+import MyCart from './MyCart';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Switch>
+          <Redirect exact path="/" to="/about" />
+          <Route path="/about" component={JerseyListContainer} />
+          <Route path="/cart" component={MyCart} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
